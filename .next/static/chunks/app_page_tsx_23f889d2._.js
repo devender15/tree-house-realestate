bcbd50f4362d1324cda0e86b52a3a50f8d61f8bc
shamisaifi <shamisaifi2003.com>
2025-05-30 15:31:30 +0530

    created branch

---
+++ .next/static/chunks/app_page_tsx_23f889d2._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_23f889d2._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_23f889d2._.js",
+  "chunks": [
+    "static/chunks/_664a0531._.js",
+    "static/chunks/node_modules_d358575e._.js"
+  ],
+  "source": "dynamic"
+});
